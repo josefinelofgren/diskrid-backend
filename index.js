dc0c8ee9e04bf30b1224444c33698cbccb73a4ba
Josefine Löfgren <josefinelofgren@Josefines-MacBook-Pro.local>
2021-11-09 17:25:05 +0100

    login and signup function

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const cors = require('cors');
 const express = require('express');
+var path = require('path');
 const app = express();
 const port = 4000;
 const host = '0.0.0.0';
 require("dotenv").config();
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// connect to database
 const MongoClient = require("mongodb").MongoClient;
 const { log } = require("console");
 
@@ -22,10 +27,11 @@ app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.json('Hello World!')
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 app.listen(port, host, () => {
     console.log("Server running on port " + port);
---
+++ routes/users.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const cors = require('cors');
+const { runInNewContext } = require('vm');
+const { ObjectId } = require('mongodb');
+const cryptoJS = require('crypto-js');
+
+
+// LOGIN FOR REGISTRATED USERS 
+router.post('/log-in', function(req, res, next){
+  
+  // find email adress
+  req.app.locals.db.collection('users').findOne({"email" : req.body.email}, function (err, result){
+    if (err){
+      console.log("ERROR");
+    
+    } if (result){
+      
+      // decrypt password from db
+      let originalPassword = cryptoJS.AES.decrypt(result.password, 'admin').toString(cryptoJS.enc.Utf8);
+      console.log(originalPassword);
+
+      // check if email and password is correct
+      if(result.email == req.body.email && originalPassword == req.body.password){
+        res.send(result)
+      } else {
+        res.send(false)
+      }
+    
+    // if email is not found 
+    } else {
+     res.send(false)
+    }
+  })
+});
+
+
+
+// ADD NEW USER 
+router.post('/sign-up', function(req, res, next) {
+
+  // crypt password 
+  let cryptoPassword = cryptoJS.AES.encrypt(req.body.password, 'admin').toString();
+  console.log(cryptoPassword);
+
+  // new user details
+  let newUser = {
+    "email": req.body.email, 
+    "password" : cryptoPassword,
+    "subscriptionStatus" : req.body.subscriptionStatus
+  }
+
+  console.log(newUser);
+  console.log(req.body.email);
+
+  // check if email exists in db 
+  req.app.locals.db.collection('users').findOne({"email" : req.body.email}, function (err, result){
+    if (err){
+    console.log("ERROR");
+  
+    // if email already exist
+    } if (result){
+      console.log("Email already exist")
+      res.send(true);
+  
+    // add new user to db 
+    } else {
+      req.app.locals.db.collection('users').insertOne(newUser)
+      .then(result => {
+        res.send(result.ops)
+
+      })
+    }
+  })
+})
+
+
+
+
+
+
+module.exports = router;
