4d683ad4ebece46020d6b96eb87f70aa7b83fbbe
Josefine Löfgren <josefinelofgren@Josefines-MacBook-Pro.local>
2021-11-09 16:50:17 +0100

    connect database

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const app = express();
 const port = 4000;
 const host = '0.0.0.0';
+require("dotenv").config();
+
+const MongoClient = require("mongodb").MongoClient;
+const { log } = require("console");
+
+MongoClient.connect(process.env.DB_CLOUD, {
+  useUnifiedTopology: true,
+}).then((client) => {
+  console.log("Database connected");
+
+  const db = client.db("users");
+  app.locals.db = db;
+});
 
 app.use(cors());
 app.options('*', cors());
